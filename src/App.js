b75bcbf7de6778bb2f97238a9635c5f66ccfcf56
吴春洋 <wuchunyang@wuchunyangdeMacBook-Pro.local>
2019-06-29 11:20:46 +0800

    antd 按需引入

--- src/App.js
+++ src/App.js
@@ -1,27 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import './es6'
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import { Button, WhiteSpace, List } from 'antd-mobile'
+class App extends React.Component{
+  constructor(){
+    super()
+    this.state = {
+      list: ['kdfsk','坏水电费','是快捷的方式']
+    }
+  }
+  componentWillMount(){
+    console.log('组件家在前')
+  }
+  componentDidMount(){
+    console.log('组件加载之后')
+  }
+  render(){
+    console.log('组件加载中....')
+    const boss = '你好'
+    return <div>react demo{boss}
+      <Button type="ghost">default</Button>
+      <WhiteSpace></WhiteSpace>
+      <List renderHeader={()=>'列表'}>
+        {this.state.list.map(v=>{
+          return <List.Item key={v}>{v}</List.Item>
+        })}
+      </List>
     </div>
-  );
+  }
 }
 
-export default App;
+export default App
